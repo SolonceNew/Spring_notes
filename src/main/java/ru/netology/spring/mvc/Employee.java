7117e0b7faaa03e316931f7792e5956294eb76b1
Anna Fabrichnaya <fabrichnaya.anna@bk.ru>
2022-03-27 20:08:25 +0300

    drop down choice

--- src/main/java/ru/netology/spring/mvc/Employee.java
+++ src/main/java/ru/netology/spring/mvc/Employee.java
@@ -1,12 +1,28 @@
 package ru.netology.spring.mvc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Employee {
     private String name;
     private String surname;
     private int salary;
     private String department;
+    Map<String, String> departments;
 
     public Employee() {
+        departments = new HashMap<>();
+        departments.put("IT", "Information Technology");
+        departments.put("HR", "Human Resources");
+        departments.put("Sales", "Sales");
+    }
+
+    public Map<String, String> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(Map<String, String> departments) {
+        this.departments = departments;
     }
 
     public String getName() {
