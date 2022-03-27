ee1fb1da673f6db0225d73cc157930747038c7e5
Anna Fabrichnaya <fabrichnaya.anna@bk.ru>
2022-03-27 17:57:31 +0300

    initial mvc

---
+++ src/main/java/ru/netology/spring/mvc/MyController.java
@@ -0,0 +1,35 @@
+package ru.netology.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MyController {
+    // @RequestMapping("showDetails")
+    //public String showEmployeeDetails() {
+      //  return "show-emp-details-view";
+    //}
+
+    @RequestMapping("/showDetails")
+    public String showEmployeeDetails(@RequestParam("employeeName")
+                                      String empName, Model model) {
+
+        empName = "Mrs. " + empName;
+        model.addAttribute("nameAttribute " + empName);
+        return "show-emp-details-view";
+    }
+
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails() {
+        return "ask-emp-details-view";
+    }
+
+    @RequestMapping("/")
+    public String showFirstView() {
+        return "first-view";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/first-view.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>I wish you luck in Spring MVC learning</h2>
+<br>
+<br>
+<a href="/askDetails">Please write your name</a>
+</body>
+</html>
